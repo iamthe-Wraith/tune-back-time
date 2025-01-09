00986daa7719dfc28f4ede4744cee025edf27d6a
iamthe-Wraith <allhailthewraith@protonmail.com>
2025-01-09 14:35:03 -0500

    feat: adds getting date from user

---
+++ user_input.py
@@ -0,0 +1,23 @@
+import datetime as dt
+
+def get_date():
+    date: dt.datetime = None
+
+    while date == None:
+        raw_date = input("Enter the date you want to travel to in the format YYYY-MM-DD: ").strip()
+
+        if raw_date == "":
+            print("[-] Date is required\n")
+            continue
+        
+        try:
+            parsed_date = dt.datetime.strptime(raw_date, "%Y-%m-%d")
+            if parsed_date > dt.datetime.now():
+                print("[-] Date must be in the past\n")
+                continue
+            date = parsed_date
+        except ValueError:
+            print("[-] Invalid date\n")
+            continue
+
+    return date
